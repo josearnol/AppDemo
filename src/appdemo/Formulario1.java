7bb4ad9e7a117c72c76f5b2baaa6b1a26f4f7081
josearnol <josearnoldo1996@hotmail.com>
2016-11-12 18:40:33 -0600

    Cambio2
    reescribier

--- src/appdemo/Formulario1.java
+++ src/appdemo/Formulario1.java
@@ -87,7 +87,7 @@ public class Formulario1 extends javax.swing.JFrame {
 
     private void btnmostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnmostrarActionPerformed
         // TODO add your handling code here:
-        JOptionPane.showMessageDialog(this,"El numero ingresado fue: " + txtnumero.getText());
+        JOptionPane.showMessageDialog(this,"El que ingreso fue: " + txtnumero.getText());
     }//GEN-LAST:event_btnmostrarActionPerformed
 
     /**
